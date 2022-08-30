c2337bfc7e08687049981d47e0dd7366f8d4f67b
hyonna12 <hyonna01@gmail.com>
2022-08-30 11:50:58 +0900

    디스패치 서블릿 직접 만들어보기. 파트1

---
+++ src/main/java/site/metacoding/ds/UserController.java
@@ -0,0 +1,14 @@
+package site.metacoding.ds;
+
+public class UserController {
+	
+	// /join 하면 호출되도록 /login 하면 호출되도록
+	public void join() {
+		System.out.println("join 호출됨");
+	}
+	
+	public void login() {
+		System.out.println("login 호출됨");
+	}
+
+}
